ea60bf8e62f6275259155a416dbea8d8a3aadbe7
Sving1024 <sving1024@outlook.com>
2024-12-29 16:51:17 +0800

    add test

--- upload/entrypoint.sh
+++ upload/entrypoint.sh
@@ -3,9 +3,15 @@ set -e
 
 init_path=$PWD
 mkdir upload_packages
+
+echo $local_path
+
 find $local_path -type f -name "*.iso" -exec cp {} ./upload_packages/ \;
+find $local_path -type f -name "*.iso" -exec echo {} \;
 find $local_path -type f -name "*.json" -exec cp {} ./upload_packages/ \;
+find $local_path -type f -name "*.json" -exec echo {} \;
 find $local_path -type f -name "*.iso.sha256.txt" -exec cp {} ./upload_packages/ \;
+find $local_path -type f -name "*.iso.sha256.txt" -exec echo {} \;
 
 echo "$RCLONE_CONFIG_NAME"
 
