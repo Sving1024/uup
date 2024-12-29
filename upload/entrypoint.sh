0938063501faf822f531161b993e87a12849a69c
Sving1024 <sving1024@outlook.com>
2024-12-29 15:24:28 +0800

    refact actions

---
+++ upload/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+init_path=$PWD
+mkdir upload_packages
+find $local_path -type f -name "*.iso" -exec cp {} ./upload_packages/ \;
+find $local_path -type f -name "*.json" -exec cp {} ./upload_packages/ \;
+find $local_path -type f -name "*.iso.sha256.txt" -exec cp {} ./upload_packages/ \;
+
+echo "$RCLONE_CONFIG_NAME"
+
+if [ ! -f ~/.config/rclone/rclone.conf ]; then
+    mkdir --parents ~/.config/rclone
+    echo "$RCLONE_CONFIG_CONTENT" >> ~/.config/rclone/rclone.conf
+fi
+
+cd upload_packages || exit 1
+
+echo "::group::Uploading to remote"
+python3 $init_path/create-db-and-upload-action/upload.py 
+echo "::endgroup::"
